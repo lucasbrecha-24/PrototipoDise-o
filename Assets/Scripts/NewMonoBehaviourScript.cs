ef4b0b0b2891206caf5c6ccaaa3f026d3ba31db9
lucasbrecha-24 <lucas.brecha@davinci.edu.ar>
2026-05-01 18:30:46 -0300

    Update NewMonoBehaviourScript.cs

--- Assets/Scripts/NewMonoBehaviourScript.cs
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -5,7 +5,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int asd = 0;   
     }
 
     // Update is called once per frame
